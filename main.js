1b065be02a26ce13caf86aab8e0ad6c9e66a3a55
Michael <pmv.michael.pmv@gmail.com>
2024-04-29 10:27:42 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+
+app.disableHardwareAcceleration(); // Отключение аппаратного ускорения
+
+function createWindow() {
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
